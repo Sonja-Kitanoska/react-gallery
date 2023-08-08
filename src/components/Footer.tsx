b820a993354c7d00207d28ec62e366afcf5dc77a
Sonja-Kitanoska <avramoskamkd@gmail.com>
2023-08-08 21:03:35 +0200

    github link changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,15 @@
-import github_logo from '../assets/github_logo.png';
+import github_logo from "../assets/github_logo.png";
 
 export const Footer = () => {
-    return (
+  return (
     <footer className="footer">
-        <a href="https://github.com/SMooreSwe" className="footer__logo"><img src={github_logo} className="footer__logo__image" alt="A logo from the website Github" /></a>
+      <a href="https://github.com/Sonja-Kitanoska" className="footer__logo">
+        <img
+          src={github_logo}
+          className="footer__logo__image"
+          alt="A logo from the website Github"
+        />
+      </a>
     </footer>
-)}
+  );
+};
